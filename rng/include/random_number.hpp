82bd0a6565247fa7619dc72fd96f9f9ddec4433e
Changmook Chun <cmookj@duck.com>
2024-07-21 02:49:22 -0700

    Implemented Alias method to sample integers in accordance to a discrete distribution given

--- rng/include/random_number.hpp
+++ rng/include/random_number.hpp
@@ -1,123 +1,212 @@
 //
 // Random Number Generation Library
 //
-// Changmook Chun. (c) 2003--2023.
-//
 // Parts of this library is written by D. E. Knuth.
 //
 
-#ifndef __RANDOM_H_
-#define __RANDOM_H_
+#ifndef _RANDOM_NUMBER_GENERATOR_H_
+#define _RANDOM_NUMBER_GENERATOR_H_
 
+#include <cmath>
 #include <memory>
+#include <vector>
+
+namespace numeric {
+
+template <typename T>
+bool
+close_enough (const T left, const T right, int max_ulps_diff = 1) {
+  bool _close_enough = false;
+
+  T lhs = std::min (left, right);
+  T rhs = std::max (left, right);
+
+  do {
+    if (lhs >= rhs) {
+      _close_enough = true;
+      break;
+    }
+    lhs = std::nextafter (lhs, INFINITY);
+  } while (max_ulps_diff--);
+
+  return _close_enough;
+}
+
+}  // namespace numeric
 
 namespace random_number {
 
-class random_int_uniform {
+// -----------------------------------------------------------------------------
+//                                              Uniform random integer generator
+// -----------------------------------------------------------------------------
+class random_int_uniform final {
 public:
-    random_int_uniform(int32_t);
-    virtual ~random_int_uniform() = default;
-    
-    random_int_uniform(const random_int_uniform&) = delete;
-    random_int_uniform(random_int_uniform&&) = default;
-    
-    random_int_uniform& operator= (const random_int_uniform&) = delete;
-    random_int_uniform& operator= (random_int_uniform&&) = default;
-    
-    // int operator() () const;
-    
-    // Do this before using _ran_array
-    // Selector for different streams
-    void ran_start (int32_t seed);
+  [[nodiscard]] random_int_uniform (int32_t);
+  virtual ~random_int_uniform() = default;
 
-    // Put n new random numbers in array
-    void ran_array (int32_t*, size_t n);
-    const int32_t& ran_x(size_t) const;
+  random_int_uniform (const random_int_uniform&) = delete;
+  random_int_uniform (random_int_uniform&&)      = default;
 
-    int ran_arr_next() {
-        return *_ran_arr_ptr >= 0 ? *_ran_arr_ptr++: _ran_arr_cycle();
-    }
+  random_int_uniform&
+  operator= (const random_int_uniform&) = delete;
+  random_int_uniform&
+  operator= (random_int_uniform&&) = default;
 
-protected:
-    static constexpr int KK = 100;        // the long lag
-    static constexpr int LL = 37;         // the short lag
-    static constexpr int MM = (1L << 30); // the modulus
-    static constexpr int QUALITY = 1009;  // recommended quality level for high-res use
-    static constexpr int TT = 70;         // guaranteed separation between streams
-    
-    int32_t _ran_arr_dummy, _ran_arr_started;
-    int32_t* _ran_arr_ptr; // the next random number, or -1
+  // int operator() () const;
 
-    std::unique_ptr<int32_t[]> _ran_arr_buf;
-    std::unique_ptr<int32_t[]> _ran_x;
+  // Do this before using _ran_array
+  // Selector for different streams
+  void
+  ran_start (int32_t seed);
 
-    int _ran_arr_cycle ();
-    
-    // Subtraction mod MM
-    int _mod_diff (const int32_t x, const int32_t y) { return (x - y) & (MM - 1); }
+  // Put n new random numbers in array
+  void
+                 ran_array (int32_t*, size_t n);
+  const int32_t& ran_x (size_t) const;
 
-    // Units bit of x
-    bool _is_odd (const int32_t x) { return x & 1; }
+  int
+  ran_arr_next () {
+    return *_ran_arr_ptr >= 0 ? *_ran_arr_ptr++ : _ran_arr_cycle();
+  }
+
+private:
+  static constexpr int KK      = 100;         // the long lag
+  static constexpr int LL      = 37;          // the short lag
+  static constexpr int MM      = (1L << 30);  // the modulus
+  static constexpr int QUALITY = 1009;        // recommended quality level for high-res use
+  static constexpr int TT      = 70;          // guaranteed separation between streams
+
+  int32_t  _ran_arr_dummy, _ran_arr_started;
+  int32_t* _ran_arr_ptr;  // the next random number, or -1
+
+  std::unique_ptr<int32_t[]> _ran_arr_buf;
+  std::unique_ptr<int32_t[]> _ran_x;
+
+  int
+  _ran_arr_cycle ();
+
+  // Subtraction mod MM
+  int
+  _mod_diff (const int32_t x, const int32_t y) {
+    return (x - y) & (MM - 1);
+  }
+
+  // Units bit of x
+  bool
+  _is_odd (const int32_t x) {
+    return x & 1;
+  }
 };
 
-class random_double_uniform {
+// -----------------------------------------------------------------------------
+//                                               Uniform random double generator
+// -----------------------------------------------------------------------------
+class random_double_uniform final {
 public:
-    random_double_uniform(int32_t);
-    virtual ~random_double_uniform() = default;
-    
-    random_double_uniform(const random_double_uniform&) = delete;
-    random_double_uniform(random_double_uniform&&) = default;
-    
-    random_double_uniform& operator= (const random_double_uniform&) = delete;
-    random_double_uniform& operator= (random_double_uniform&&) = default;
-    
-    // double operator() () const;
+  [[nodiscard]] random_double_uniform (int32_t);
+  virtual ~random_double_uniform() = default;
 
-    // Do this before using _ranf_array
-    // Selector for different streams
-    void ranf_start (int32_t seed);
+  random_double_uniform (const random_double_uniform&) = delete;
+  random_double_uniform (random_double_uniform&&)      = default;
 
-    // Put n new random fractions in aa
-    void ranf_array (double* aa, size_t n);
-    const double& ran_u(size_t) const;
-    
-    double ranf_arr_next() {
-        return *_ranf_arr_ptr >= 0 ? *_ranf_arr_ptr++ : _ranf_arr_cycle();
-    }
+  random_double_uniform&
+  operator= (const random_double_uniform&) = delete;
+  random_double_uniform&
+  operator= (random_double_uniform&&) = default;
 
-protected:
-    static constexpr int KK = 100;        // the long lag
-    static constexpr int LL = 37;         // the short lag
-    static constexpr int QUALITY = 1009;  // recommended quality level for high-res use
-    static constexpr int TT = 70;         // guaranteed separation between streams
-    
-    std::unique_ptr<double[]> _ran_u = std::make_unique<double[]> (KK); // the generator state
-    std::unique_ptr<double[]> _ranf_arr_buf = std::make_unique<double[]> (QUALITY);
-    double _ranf_arr_dummy = -1.0, _ranf_arr_started=-1.0;
-    double* _ranf_arr_ptr = &_ranf_arr_dummy; // the next random fraction, or -1
+  // double operator() () const;
 
-    // (x+y) mod 1.0
-    double _mod_sum (const double x, const double y) {
-        return (x + y) - static_cast<int> (x + y);
-    }
+  // Do this before using _ranf_array
+  // Selector for different streams
+  void
+  ranf_start (int32_t seed);
 
-    double _ranf_arr_cycle ();
-    
-    // Units bit of x
-    bool _is_odd (const int32_t x) { return x & 1; }
+  // Put n new random fractions in aa
+  void
+                ranf_array (double* aa, size_t n);
+  const double& ran_u (size_t) const;
+
+  double
+  ranf_arr_next () {
+    return *_ranf_arr_ptr >= 0 ? *_ranf_arr_ptr++ : _ranf_arr_cycle();
+  }
+
+private:
+  static constexpr int KK      = 100;   // the long lag
+  static constexpr int LL      = 37;    // the short lag
+  static constexpr int QUALITY = 1009;  // recommended quality level for high-res use
+  static constexpr int TT      = 70;    // guaranteed separation between streams
+
+  std::unique_ptr<double[]> _ran_u        = std::make_unique<double[]> (KK);  // the generator state
+  std::unique_ptr<double[]> _ranf_arr_buf = std::make_unique<double[]> (QUALITY);
+  double                    _ranf_arr_dummy = -1.0, _ranf_arr_started = -1.0;
+  double*                   _ranf_arr_ptr = &_ranf_arr_dummy;  // the next random fraction, or -1
+
+  // (x+y) mod 1.0
+  double
+  _mod_sum (const double x, const double y) {
+    return (x + y) - static_cast<int> (x + y);
+  }
+
+  double
+  _ranf_arr_cycle ();
+
+  // Units bit of x
+  bool
+  _is_odd (const int32_t x) {
+    return x & 1;
+  }
 };
 
-//// normal dist.
-//int dist_normal_polar_rejection (double*, int n);
-//
-//// gamma dist.
-//int dist_gamma (double*, int n, double a, double r);
-//
-//// Brownian bridge
-//int brownian_bridge_bisection (
-//    double*, double*, double z_0, double z_N, int N
-//);
+// -----------------------------------------------------------------------------
+//                           Random integer generator with discrete distribution
+// -----------------------------------------------------------------------------
+class random_int_discrete_distribution final {
+public:
+  [[nodiscard]] random_int_discrete_distribution (
+      const std::vector<double>& prob,
+      const int32_t              seed
+  );
+  virtual ~random_int_discrete_distribution() = default;
 
-} // namespace RandomNumber
+  random_int_discrete_distribution (const random_int_discrete_distribution&) = delete;
+  random_int_discrete_distribution (random_int_discrete_distribution&&)      = default;
 
-#endif // __RANDOM_H_
+  random_int_discrete_distribution&
+  operator= (const random_int_discrete_distribution&) = delete;
+  random_int_discrete_distribution&
+  operator= (random_int_discrete_distribution&&) = delete;
+
+  int
+  operator() () const;
+
+private:
+  void
+  generate_table (const std::vector<double>& prob);
+
+  void
+  print_table () const;
+
+  const int max_rnd_number;
+  enum class level { overfull, underfull, exactly_full };
+  struct table_entry {
+    int    i;
+    double acceptance;  // U
+    int    alias;       // K
+    level  fill_level;
+  };
+  std::vector<table_entry> table;
+
+  mutable random_double_uniform random_uniform_double_generator;
+
+  // -------------------------------------
+  //  Appendix: for debugging purpose only
+  // -------------------------------------
+public:
+  const std::vector<table_entry>&
+  tbl () const;
+};
+
+}  // namespace random_number
+
+#endif
--- rng/src/random_number.cpp
+++ rng/src/random_number.cpp
@@ -1,20 +1,25 @@
 //
 // Random Number Generation Library
 //
-// Changmook Chun. (c) 2003--2023.
-//
 // Parts of this library is written by D. E. Knuth.
 //
 
 #include "random_number.hpp"
 
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+// #include <math.h>
+// #include <stdlib.h>
+// #include <time.h>
+#include <numeric>
 #include <type_traits>
 
 namespace random_number {
 
+// -----------------------------------------------------------------------------
+//                                                            random_int_uniform
+// -----------------------------------------------------------------------------
 #pragma mark - Uniform Random Integer Generator
 
 random_int_uniform::random_int_uniform (int32_t seed)
@@ -35,10 +40,13 @@ random_int_uniform::ran_array (int32_t* aa, size_t n) {
   size_t i, j;
   for (j = 0; j < KK; j++)
     aa[j] = _ran_x[j];
+
   for (; j < n; j++)
     aa[j] = _mod_diff (aa[j - KK], aa[j - LL]);
+
   for (i = 0; i < LL; i++, j++)
     _ran_x[i] = _mod_diff (aa[j - KK], aa[j - LL]);
+
   for (; i < KK; i++, j++)
     _ran_x[i] = _mod_diff (aa[j - KK], _ran_x[i - LL]);
 }
@@ -48,33 +56,42 @@ random_int_uniform::ran_start (int seed) {
   int     t, j;
   auto    x  = std::make_unique<int32_t[]> (KK + KK - 1);  // the preparation buffer
   int32_t ss = (seed + 2) & (MM - 2);
+
   for (j = 0; j < KK; j++) {
     x[j] = ss;  // bootstrap the buffer
     ss <<= 1;
     if (ss >= MM) ss -= MM - 2;  // cyclic shift 29 bits
   }
+
   x[1]++;  // make x[1] (and only x[1]) odd
   for (ss = seed & (MM - 1), t = TT - 1; t;) {
     for (j = KK - 1; j > 0; j--)
       x[j + j] = x[j], x[j + j - 1] = 0;  // "square"
+
     for (j = KK + KK - 2; j >= KK; j--)
       x[j - (KK - LL)]      = _mod_diff (x[j - (KK - LL)], x[j]),
                   x[j - KK] = _mod_diff (x[j - KK], x[j]);
+
     if (_is_odd (ss)) {  // "multiply by z"
       for (j = KK; j > 0; j--)
         x[j] = x[j - 1];
+
       x[0]  = x[KK];  // shift the buffer cyclically
       x[LL] = _mod_diff (x[LL], x[KK]);
     }
     if (ss) ss >>= 1;
     else t--;
   }
+
   for (j = 0; j < LL; j++)
     _ran_x[j + KK - LL] = x[j];
+
   for (; j < KK; j++)
     _ran_x[j - LL] = x[j];
+
   for (j = 0; j < 10; j++)
     ran_array (x.get(), KK + KK - 1);  // warm things up
+
   _ran_arr_ptr = &_ran_arr_started;
 }
 
@@ -93,6 +110,9 @@ random_int_uniform::ran_x (size_t i) const {
   return (i < KK ? _ran_x[i] : _ran_x[0]);
 }
 
+// -----------------------------------------------------------------------------
+//                                                         random_double_uniform
+// -----------------------------------------------------------------------------
 #pragma mark - Uniform Random Fraction Generator
 
 random_double_uniform::random_double_uniform (int32_t seed)
@@ -121,37 +141,44 @@ random_double_uniform::ranf_array (double* aa, size_t n) {
   size_t i, j;
   for (j = 0; j < KK; j++)
     aa[j] = _ran_u[j];
+
   for (; j < n; j++)
     aa[j] = _mod_sum (aa[j - KK], aa[j - LL]);
+
   for (i = 0; i < LL; i++, j++)
     _ran_u[i] = _mod_sum (aa[j - KK], aa[j - LL]);
+
   for (; i < KK; i++, j++)
     _ran_u[i] = _mod_sum (aa[j - KK], _ran_u[i - LL]);
 }
 
 void
 random_double_uniform::ranf_start (int32_t seed) {
-  int    t, s, j;
-  auto   u   = std::make_unique<double[]> (KK + KK - 1);
-  double ulp = (1.0 / (1L << 30)) / (1L << 22);  // 2 to the -52
-  double ss  = 2.0 * ulp * ((seed & 0x3fffffff) + 2);
+  int              t, s, j;
+  auto             u   = std::make_unique<double[]> (KK + KK - 1);
+  constexpr double ulp = (1.0 / (1L << 30)) / (1L << 22);  // 2 to the -52
+  double           ss  = 2.0 * ulp * ((seed & 0x3fffffff) + 2);
 
   for (j = 0; j < KK; j++) {
     u[j] = ss;  // bootstrap the buffer
     ss += ss;
     if (ss >= 1.0) ss -= 1.0 - 2 * ulp;  // cyclic shift of 51 bits
   }
+
   u[1] += ulp;  // make u[1] (and only u[1]) "odd"
   for (s = seed & 0x3fffffff, t = TT - 1; t;) {
     for (j = KK - 1; j > 0; j--)
       u[j + j] = u[j], u[j + j - 1] = 0.0;  // "square"
+
     for (j = KK + KK - 2; j >= KK; j--) {
       u[j - (KK - LL)] = _mod_sum (u[j - (KK - LL)], u[j]);
       u[j - KK]        = _mod_sum (u[j - KK], u[j]);
     }
+
     if (_is_odd (s)) {  // "multiply by z"
       for (j = KK; j > 0; j--)
         u[j] = u[j - 1];
+
       u[0]  = u[KK];  // shift the buffer cyclically
       u[LL] = _mod_sum (u[LL], u[KK]);
     }
@@ -160,10 +187,13 @@ random_double_uniform::ranf_start (int32_t seed) {
   }
   for (j = 0; j < LL; j++)
     _ran_u[j + KK - LL] = u[j];
+
   for (; j < KK; j++)
     _ran_u[j - LL] = u[j];
+
   for (j = 0; j < 10; j++)
     ranf_array (u.get(), KK + KK - 1);  // warm things up
+
   _ranf_arr_ptr = &_ranf_arr_started;
 }
 
@@ -180,6 +210,98 @@ random_double_uniform::_ranf_arr_cycle() {
 const double&
 random_double_uniform::ran_u (size_t i) const {
   return (i < KK ? _ran_u[i] : _ran_u[0]);
+}
+
+// -----------------------------------------------------------------------------
+//                                              random_int_discrete_distribution
+// -----------------------------------------------------------------------------
+#pragma mark - Random Integer Generator with Discrete Distribution
+
+void
+random_int_discrete_distribution::print_table() const {
+  for (const auto& entry : table) {
+    std::cout << "i = " << entry.i << ", Ui = " << entry.acceptance << ", Ki = " << entry.alias
+              << " - "
+              << (entry.fill_level == level::exactly_full
+                      ? "F"
+                      : (entry.fill_level == level::overfull ? "O" : "U"))
+              << '\n';
+  }
+}
+
+random_int_discrete_distribution::random_int_discrete_distribution (
+    const std::vector<double>& prob,
+    const int32_t              seed
+)
+    : max_rnd_number{static_cast<int> (prob.size())}
+    , random_uniform_double_generator{seed} {
+  generate_table (prob);
+}
+
+void
+random_int_discrete_distribution::generate_table (const std::vector<double>& prob) {
+  // Initialize U(i) = n p(i)
+  double sum = std::accumulate (prob.cbegin(), prob.cend(), 0.0);
+  if (numeric::close_enough (sum, 0.0)) return;
+
+  const int n = max_rnd_number;
+  table.reserve (n);
+
+  int i = 0;
+  std::transform (
+      prob.cbegin(),
+      prob.cend(),
+      std::back_inserter (table),
+      [n, sum, &i] (const double p) {
+        double u = n * p / sum;
+
+        auto fill_level = level::exactly_full;
+        if (u > 1.0) fill_level = level::overfull;
+        if (u < 1.0) fill_level = level::underfull;
+
+        i++;
+        int k = (fill_level == level::exactly_full ? i : 0);
+
+        return table_entry{i, u, k, fill_level};
+      }
+  );
+
+  // Update the table
+  for (int i = 0; i < n; ++i) {
+    auto entry_i = std::find_if (table.begin(), table.end(), [] (const auto& e) {
+      return e.fill_level == level::overfull;
+    });
+
+    auto entry_j = std::find_if (table.begin(), table.end(), [] (const auto& e) {
+      return e.fill_level == level::underfull;
+    });
+
+    if (entry_j == table.end()) break;
+
+    entry_j->alias = entry_i->i;
+    entry_i->acceptance += (entry_j->acceptance - 1.);
+    entry_j->fill_level = level::exactly_full;
+
+    if (entry_i->acceptance < 1.0) entry_i->fill_level = level::underfull;
+
+    if (numeric::close_enough (entry_i->acceptance, 1.0)) entry_i->fill_level = level::exactly_full;
+  }
+}
+
+int
+random_int_discrete_distribution::operator() () const {
+  double       random_double = random_uniform_double_generator.ranf_arr_next();
+  const int    n             = max_rnd_number;
+  const int    i             = std::floor (random_double * n) + 1;
+  const double y             = random_double * n + 1 - i;
+
+  if (y < table[i - 1].acceptance) return i;
+  return table[i - 1].alias;
+}
+
+const std::vector<random_int_discrete_distribution::table_entry>&
+random_int_discrete_distribution::tbl() const {
+  return table;
 }
 
 // *****************************************************************************
